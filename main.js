86ae6851dcd09098b84b943c99090f7844bdd813
sapphiren <117869067+sapphiren@users.noreply.github.com>
2023-08-16 17:40:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized.')
+}
+function draw() {
+    background('#969A97');
+    textSize(difference);
+    text("Naba", 30, 150);
+
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
